efb09dd97ca19bfbce93aa31457e026a693d0d22
eunbean-ops <rida1223@naver.com>
2026-03-20 13:23:17 +0900

    3-5

---
+++ code/3-5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<stdarg.h>
+
+void my_sum(int type, int count,...){
+    va_list ap;
+    va_start(ap,count);
+    int sum=0; 
+
+    if(type== 'S'){
+        for(int i=0; i<count; i++){
+            printf("%s ", va_arg(ap, char*));
+        }
+        printf("\n");
+    }else if(type == 'C'){
+        for(int i=0; i<count; i++){
+            printf("%c", va_arg(ap, int));
+        }
+        printf("\n");
+    }else if( type =='D'){
+        for(int i=0; i<count; i++ ){
+            sum+= va_arg(ap, int);
+        }
+        printf("%d", sum);
+    }
+}
+
+int main(){
+    my_sum('S',2,"Hello","World");
+    my_sum('C', 3, 'C','N','U');
+    my_sum('D',4,10,20,30,40);
+    return 0;
+}
