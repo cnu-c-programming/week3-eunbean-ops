54d6793e6cc6018f120b3c528bcd2bdc6e1a0b5e
eunbean-ops <rida1223@naver.com>
2026-03-20 09:32:40 +0900

    1-2

---
+++ code/1-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+
+void print(){
+    printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+int square(int n){
+    return n*n;
+}
+
+    print();
+
+    int num= 5;
+    int result= square(num);
+    printf("%d %d\n", num, result);
+
+    return 0;
+}
